686908d9bf0af4c10d8d482ac6d2cf00a88515e0
Iwan Nusirwan <wonkinter@gmail.com>
2019-05-27 23:12:41 +0700

    Start styling PaletteList withStyles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,20 +1,58 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/styles';
 // import { Link } from "react-router-dom";
 
 import MiniPalette from './MiniPalette';
 
+const styles = {
+    root: {
+        backgroundColor: "blue",
+        height: "100%",
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "center",
+    },
+    container: {
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+        flexWrap: "wrap",
+    },
+    nav: {
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center",
+        color: "white",
+    },
+      palettes: {
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat( 3, 30% )",
+        gridGap: "2.5rem",
+    }
+};
+
 class PaletteList extends Component {
     render() {
-        const { palettes } = this.props;
+        const { classes, palettes } = this.props;
         return (
-            <div>
-                <h1>React Colors</h1>
-                { palettes.map( palette => (
-                    <MiniPalette { ...palette } />
-                ))}
+            <div className={ classes.root }>
+                <div className={ classes.container }>
+                    <nav className={ classes.nav }>
+                        <h1>React Colors</h1>
+                    </nav>
+                </div>
+                <div className={ classes.palettes }>
+                    { palettes.map( palette => (
+                        <MiniPalette { ...palette } />
+                    ))}
+                </div>
             </div>
         );
     }
 }
 
-export default PaletteList;
+export default withStyles( styles )( PaletteList );
