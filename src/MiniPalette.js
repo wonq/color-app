c0d153d742dcef8b34415267931fa5d23fbd547c
Iwan Nusirwan <wonkinter@gmail.com>
2019-05-27 23:00:17 +0700

    Start styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,22 +2,49 @@ import React from 'react';
 import { withStyles } from '@material-ui/styles';
 
 const styles = {
-    main: {
-        backgroundColor: "purple",
-        border: "3px solid teal",
-
+    root: {
+        backgroundColor: "white",
+        border: "1px solid black",
+        borderRadius: "5px",
+        padding: "0.5rem",
+        position: "relative",
+        overflow: "hidden",
+        cursor: "pointer",
+        "&:hover svg": {
+          opacity: 1
+        }
     },
-    secondary: {
-        backgroundColor: "pink",
-    }
+    colors: {
+        backgroundColor: "#dae1e4",
+        height: "150px",
+        width: "100%",
+        borderRadius: "5px",
+        overflow: "hidden"
+    },
+    title: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: "0",
+        color: "black",
+        paddingTop: "0.5rem",
+        fontSize: "1rem",
+        position: "relative"
+    },
+    emoji: {
+        marginLeft: "0.5rem",
+        fontSize: "1.5rem"
+    },
 };
 
 function MiniPalette( props ) {
-    const { classes } = props;
+    const { classes, paletteName, emoji } = props;
     return (
-        <div className={ classes.main }>
-            <h1>Mini Palette</h1>
-            <section className={ classes.secondary }>asdfasdf</section>
+        <div className={ classes.root }>
+            <div className={ classes.colors }></div>
+            <h5 className={ classes.title }>
+                { paletteName } <span className={ classes.title }>{ emoji }</span>
+            </h5>
         </div>
     );
 };
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 import MiniPalette from './MiniPalette';
 
@@ -8,12 +8,9 @@ class PaletteList extends Component {
         const { palettes } = this.props;
         return (
             <div>
-                <MiniPalette />
                 <h1>React Colors</h1>
                 { palettes.map( palette => (
-                    <p>
-                        <Link to={`/palette/${ palette.id }`}>{ palette.paletteName }</Link>
-                    </p>
+                    <MiniPalette { ...palette } />
                 ))}
             </div>
         );
