77d33baebc8ed0da269e25e5aa856d87586ca816
Iwan Nusirwan <wonkinter@gmail.com>
2019-05-29 02:15:21 +0700

    Refactor Navbar Styles and Removing CSS

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,8 +7,9 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import { withStyles } from '@material-ui/styles';
 
-import './Navbar.css';
+import styles from './styles/NavbarStyles';
 
 class Navbar extends Component {
     constructor(props) {
@@ -34,18 +35,18 @@ class Navbar extends Component {
     }
 
     render() {
-        const { lavel, changeLavel, showAllColors } = this.props;
+        const { lavel, changeLavel, showAllColors, classes } = this.props;
         const { format, open } = this.state;
 
         return (
-            <header className="Navbar">
-                <div className="logo">
+            <header className={ classes.Navbar }>
+                <div className={ classes.logo }>
                     <Link to="/">react color picker</Link>
                 </div>
                 { showAllColors &&
-                    <div className="slider-container">
+                    <div>
                         <span>Lavel: { lavel }</span>
-                        <div className="slider">
+                        <div className={ classes.slider }>
                             <Slider
                                 defaultValue={ lavel }
                                 min={ 100 }
@@ -56,7 +57,7 @@ class Navbar extends Component {
                         </div>
                     </div>
                 }
-                <div className="select-container">
+                <div className={ classes.selectContainer }>
                     <Select value={ format } onChange={ this.handleFormatChange }>
                         <MenuItem value="hex">HEX - #ffffff</MenuItem>
                         <MenuItem value="rgb">RGB - rgb( 255, 255, 255 )</MenuItem>
@@ -90,4 +91,4 @@ class Navbar extends Component {
     }
 }
 
-export default Navbar;
+export default withStyles( styles )( Navbar );
