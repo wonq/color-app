77879a7997b9a456da1f87067d971cb873517ff3
Iwan Nusirwan <wonkinter@gmail.com>
2019-06-04 04:15:20 +0700

    CleanUp Files

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -7,8 +7,8 @@ import classNames from "classnames";
 import styles from './styles/ColorBoxStyles';
 
 class ColorBox extends Component {
-    constructor(props) {
-        super(props);
+    constructor( props ) {
+        super( props );
         this.state = { copied: false }
         this.changeCopyState = this.changeCopyState.bind( this );
     }
@@ -17,7 +17,7 @@ class ColorBox extends Component {
         this.setState(
             { copied: true },
             () => { setTimeout( () =>
-                this.setState({copied: false})
+                this.setState({ copied: false })
                 , 1500)
             }
         )
@@ -53,7 +53,7 @@ class ColorBox extends Component {
                         </button>
                     </div>
                     { showingFullPalette &&
-                        <Link to={ moreUrl } onClick={ e => e.stopPropagation() }>
+                        <Link to={ moreUrl } onClick={ event => event.stopPropagation() }>
                             <span className={ classes.seeMore }>
                                 More
                             </span>
