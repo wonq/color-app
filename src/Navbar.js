8fcce5809e1b43e028bcd69884e50f5edfdc059b
Iwan Nusirwan <wonkinter@gmail.com>
2019-06-05 00:23:26 +0700

    Navbar: refactory & Using Hooks

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
@@ -11,84 +11,76 @@ import { withStyles } from '@material-ui/styles';
 
 import styles from './styles/NavbarStyles';
 
-class Navbar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            format: "hex",
-            open: false
-        }
-        this.handleFormatChange = this.handleFormatChange.bind( this );
-        this.closeSnackbar = this.closeSnackbar.bind( this );
-    }
+const Navbar = ( props ) => {
+    const [ format, setFormat ] = useState( "hex" );
+    const [ open, setOpen ] = useState( false );
 
-    handleFormatChange( event ) {
-        this.setState({
-            format: event.target.value,
-            open: true
-        });
-        this.props.handleChange( event.target.value )
-    }
+    const {
+        handleChange,
+        lavel,
+        changeLavel,
+        showAllColors,
+        classes
+    } = props;
 
-    closeSnackbar() {
-        this.setState({ open: false });
-    }
+    const handleFormatChange = ( event ) => {
+        setFormat( event.target.value );
+        setOpen( true );
+        handleChange( event.target.value );
+    };
 
-    render() {
-        const { lavel, changeLavel, showAllColors, classes } = this.props;
-        const { format, open } = this.state;
+    const closeSnackbar = () => setOpen( false );
 
-        return (
-            <header className={ classes.Navbar }>
-                <div className={ classes.logo }>
-                    <Link to="/">react color picker</Link>
-                </div>
-                { showAllColors &&
-                    <div>
-                        <span>Lavel: { lavel }</span>
-                        <div className={ classes.slider }>
-                            <Slider
-                                defaultValue={ lavel }
-                                min={ 100 }
-                                max={ 900 }
-                                onAfterChange={ changeLavel }
-                                step={ 100 }
-                            />
-                        </div>
+    return (
+        <header className={ classes.Navbar }>
+            <div className={ classes.logo }>
+                <Link to="/">react color picker</Link>
+            </div>
+            { showAllColors &&
+                <div>
+                    <span>Lavel: { lavel }</span>
+                    <div className={ classes.slider }>
+                        <Slider
+                            defaultValue={ lavel }
+                            min={ 100 }
+                            max={ 900 }
+                            onAfterChange={ changeLavel }
+                            step={ 100 }
+                        />
                     </div>
-                }
-                <div className={ classes.selectContainer }>
-                    <Select value={ format } onChange={ this.handleFormatChange }>
-                        <MenuItem value="hex">HEX - #ffffff</MenuItem>
-                        <MenuItem value="rgb">RGB - rgb( 255, 255, 255 )</MenuItem>
-                        <MenuItem value="rgba">RGBA - rgba( 255, 255, 255, 1.0 )</MenuItem>
-                    </Select>
                 </div>
-                <Snackbar
-                    anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-                    open={ open }
-                    autoHideDuration={ 3000 }
-                    message={
-                        <span id="message-id">
-                            Format Changed To { format.toUpperCase() }
-                        </span>
-                    }
-                    ContentProps={{ "aria-describedby": "message-id" }}
-                    onClose={ this.closeSnackbar }
-                    action={[
-                        <IconButton
-                            onClick={ this.closeSnackbar }
-                            color="inherit"
-                            key="close"
-                            aria-label="close"
-                        >
-                            <CloseIcon />
-                        </IconButton>
-                    ]}
-                />
-            </header>
-        );
-    }
-}
+            }
+            <div className={ classes.selectContainer }>
+                <Select value={ format } onChange={ handleFormatChange }>
+                    <MenuItem value="hex">HEX - #ffffff</MenuItem>
+                    <MenuItem value="rgb">RGB - rgb( 255, 255, 255 )</MenuItem>
+                    <MenuItem value="rgba">RGBA - rgba( 255, 255, 255, 1.0 )</MenuItem>
+                </Select>
+            </div>
+            <Snackbar
+                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                open={ open }
+                autoHideDuration={ 3000 }
+                message={
+                    <span id="message-id">
+                        Format Changed To { format.toUpperCase() }
+                    </span>
+                }
+                ContentProps={{ "aria-describedby": "message-id" }}
+                onClose={ closeSnackbar }
+                action={[
+                    <IconButton
+                        onClick={ closeSnackbar }
+                        color="inherit"
+                        key="close"
+                        aria-label="close"
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                ]}
+            />
+        </header>
+    );
+};
 
 export default withStyles( styles )( Navbar );
